2d67f1f73e0d9a81cc0eac7bc6b9cdb09cd08fb5
myoshito <marubomu@gmail.com>
2025-02-24 21:43:37 +0900

    Back to original secocnd take

--- src/test1/YutaTest.java
+++ src/test1/YutaTest.java
@@ -4,11 +4,6 @@ public class YutaTest {
 
 	public static void main(String[] args) {
 		System.out.println("こんにちは");
-<<<<<<< HEAD
-=======
-		
-		OtherGreeting.Bye();
->>>>>>> 8cb1646b056e08745195ef3d433bda2cb18ef774
 	}
 }
 
