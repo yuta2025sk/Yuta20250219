59ec526371acfa7c8ee31dcbd4d71518b509d1d2
myoshito <marubomu@gmail.com>
2025-02-24 21:08:22 +0900

    br_yoshito

---
+++ src/test1/OtherGreeting.java
@@ -0,0 +1,9 @@
+package test1;
+
+public class OtherGreeting {
+	static String bye = "また会いましょう";
+	
+	public static void Bye() {
+	System.out.println(bye);
+	}
+}
--- src/test1/YutaTest.java
+++ src/test1/YutaTest.java
@@ -4,7 +4,8 @@ public class YutaTest {
 
 	public static void main(String[] args) {
 		System.out.println("こんにちは");
-
+		
+		OtherGreeting.Bye();
 	}
-
 }
+
